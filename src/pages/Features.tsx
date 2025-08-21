dae2842ecd89f9bc43fa3f48f01b57e7f96e6839
tarekul42 <tarekulrifat205@gmail.com>
2025-08-21 12:48:34 +0600

    basic routes added for public pages

---
+++ src/pages/Features.tsx
@@ -0,0 +1,9 @@
+const Features = () => {
+  return (
+    <div>
+      <h1>This is Features component</h1>
+    </div>
+  );
+};
+
+export default Features;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,41 @@
 import App from "@/App";
-import About from "@/pages/About/About";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+import FAQ from "@/pages/FAQ";
+import Features from "@/pages/Features";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
-    {
-        path: "/", 
-        Component: App,
-        children: [
-            {
-                path: "/",
-                Component: About
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        path: "about",
+        Component: About,
+      },
+      {
+        path: "features",
+        Component: Features,
+      },
+      {
+        path: "contact",
+        Component: Contact,
+      },
+      {
+        path: "faq",
+        Component: FAQ,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
+  },
+]);
